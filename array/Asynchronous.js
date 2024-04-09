c5717283fbba59f97789fdedfbc85aba1c2c2f70
shubham <shubhampandeyup07@gmail.com>
2024-04-09 23:48:34 +0530

    Asynchronous code

---
+++ array/Asynchronous.js
@@ -0,0 +1,19 @@
+//Asynchronous funtion
+
+// console.log('hello')
+
+// console.log('javascript')
+
+// setTimeout(function(){
+//     console.log('I am from setTimeout.')
+// },3000)
+
+
+
+//
+
+function greeting(){
+    console.log('I am from setTimeout')
+}
+setTimeout(greeting, 3000)
+console.log('javascript')

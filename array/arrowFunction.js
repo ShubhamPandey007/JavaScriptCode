5b0ae86393a237b9ebc30767f0fbc3326de91d6b
shubham <shubhampandeyup07@gmail.com>
2024-04-09 23:47:54 +0530

    arrow Function code

---
+++ array/arrowFunction.js
@@ -0,0 +1,24 @@
+// normal function
+// const login = function(){
+//     console.log('logged in');
+// }
+// login();
+
+//arrow function
+
+// const login = () => {
+//     console.log('logged in');
+
+// }
+// login();
+
+
+//number sum use to arrow function
+
+const sum = (num1, num2) => {
+    
+    return num1 + num2;
+}
+
+const result = sum(4, 6);
+console.log(result);

d6763363180f6a22d0457cc43654b0db68adac51
shubham <shubhampandeyup07@gmail.com>
2024-04-09 23:49:05 +0530

    function code

---
+++ array/function.js
@@ -0,0 +1,35 @@
+// function upperCase(str){
+//     console.log(str.toUpperCase());
+// }
+
+// upperCase('javascript');
+
+
+//return function
+
+// function upperCase(strd){
+//     return strd.toUpperCase();
+// }
+// const result = upperCase('javascript');
+// console.log(result);
+
+
+
+ //calculateArea
+
+//  function calculateArea(width, height){
+//     const area = width * height;
+//     return area;
+//  }
+//  const area = calculateArea(30,40);
+//  console.log(area); 
+
+
+//variables scope
+
+const filename = 'xyz.pdf';
+function download(){
+    console.log(fileName)
+}
+download();
+

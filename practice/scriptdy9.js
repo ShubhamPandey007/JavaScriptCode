bd8cd7d616204a6960ed68ea4d5524819c285fd0
shubham <shubhampandeyup07@gmail.com>
2024-04-13 23:33:43 +0530

    javascript practice code

---
+++ practice/scriptdy9.js
@@ -0,0 +1,7 @@
+// Mathmetical operators
+
+const width = prompt("Please enter a Rectangle Width");
+const height = prompt("Please enter a Rectangle Height")
+
+console.log(width * height);
+console.log(`Rectangle Area: ${width * height}`)

9ee01c60992412fdceb6bbccc9a8a32941d6e1c8
shubham <shubhampandeyup07@gmail.com>
2024-04-13 23:33:12 +0530

    javascript practice code

---
+++ practice/scriptdy6.js
@@ -0,0 +1,10 @@
+console.log(firstName)
+var firstName = 'shubham';
+let lastName = "pandey"
+const age = 30;
+let birthDate = 24;
+const yearofBirth = 1994;
+
+// let userIntro = 'Hii, my name is ' + firstName + lastName + 'and my age is ' + age;
+
+

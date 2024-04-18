6d28350517bcb8bb5e9f1db238b9e81ad251c78e
shubham <shubhampandeyup07@gmail.com>
2024-04-18 23:58:55 +0530

    js practice code

---
+++ New folder/practice.js
@@ -0,0 +1,59 @@
+// nested for loops
+
+// first loop
+
+// debugger;
+// for (let i = 1; i <= 3; i++) {
+
+//     // second loop
+//     for (let j = 1; j <= 3; j++) {
+//         if (i == 2) {
+//           break;
+//         }
+//         console.log(`i = ${i}, j = ${j}`);
+//     }
+// }
+
+// debugger;
+// for(let i=0;i<3;i++){
+//     console.log( "i", i)
+
+//     for (let j=0;j<3;j++){
+//         console.log("j", j)
+//         if(j==1){
+//             break;
+//         }
+//     }
+// }
+
+
+// program to print the value of i
+
+// for (let i = 1; i <= 5; i++) {
+
+//     // condition to continue    
+//     if (i == 3) {
+//         continue;
+//     }
+
+//     console.log(i);
+// }
+
+
+// nested for loops
+
+// first loop
+
+// for (let i = 1; i <= 3; i++) {
+
+//     // second loop
+//     for (let j = 1; j <= 3; j++) {
+//         if (j == 2) {
+//           continue;
+//         }
+//         console.log(`i = ${i}, j = ${j}`);
+//     }
+// }
+
+
+

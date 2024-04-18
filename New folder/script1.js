bf33957644b14cdd21e62f2d8a407cd7d347c7b5
shubham <shubhampandeyup07@gmail.com>
2024-04-18 23:57:47 +0530

    this is js practice code

---
+++ New folder/script1.js
@@ -0,0 +1,31 @@
+for(let i=0;i<10;i++); {console.log("Hii");}
+
+for(let i=0;i<10;i++)
+ {console.log("Hii");}
+
+ //
+
+ 
+for(let i=0;i<3;i++){
+    console.log( "i", i)
+
+    for (let j=0;j<3;j++){
+        console.log("j", j)
+        if(j==1){
+            break;
+        }
+    }
+}
+ ///
+
+ 
+for(let i = 0 ;i<4;i++){
+    console.log(i);
+    if(i==2){
+        continue;
+    }
+    console.log(i*20)
+}
+  
+0,0,1,20, 2, 3, 60
+

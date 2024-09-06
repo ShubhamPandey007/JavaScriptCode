63e2f841f911974aac6c653c196cac8d2d51bc29
shubham <shubhampandeyup07@gmail.com>
2024-09-06 13:59:54 +0530

    Memory creation concept

---
+++ practice/Js6MemoryCreation.js
@@ -0,0 +1,13 @@
+console.log(firstName)
+
+console.log(lastName)
+var firstName = 'shubham';
+let lastName = "pandey"
+const age = 30;
+let birthDate = 24;
+const yearofBirth = 1994;
+
+// let userIntro = 'Hii, my name is ' + firstName + lastName + 'and my age is ' + age;
+
+//TZD = is the period of time during which the let and const declaration cannot be accessed.
+

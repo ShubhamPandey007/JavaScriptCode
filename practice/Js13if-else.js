09220e2a10aa76be67dfeb8f1b8dff40085c44dc
shubham <shubhampandeyup07@gmail.com>
2024-09-07 20:01:11 +0530

    if-else concept code

---
+++ practice/Js13if-else.js
@@ -0,0 +1,20 @@
+const userName = 'Shubham';
+const userAge= 24;
+
+// const userName = prompt('Enter your name:');
+// const userAge = prompt('Enter your age:');
+
+
+console.log(`Name: ${userName}`)
+console.log(`Age: ${userAge}`);
+
+console.log(`Name: ${userName} and age: ${userAge} `)
+
+
+
+if(userAge >= 18 && userAge <= 45){
+    console.log(`User Name is ${userName} and his age is ${userAge} and he is a college student`);
+}else {
+console.log('User is cross the student list  ')
+}
+

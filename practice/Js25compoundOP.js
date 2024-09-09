77006ecd911892e2345d513a8f09fc40dd1215ad
shubham <shubhampandeyup07@gmail.com>
2024-09-09 14:13:47 +0530

    compound Operater code conpect

---
+++ practice/Js25compoundOP.js
@@ -0,0 +1,46 @@
+console.log('compund (Combined Assignment) Operators');
+
+let num = 5;
+
+num = num + 2;
+num += 2; // num me + karo num + 2.
+// console.log(num)
+
+num *= 6
+console.log(num)
+
+let rem = 44
+rem %= 5  // Remainder Assignments operator
+console.log(rem)
+
+let Div = 88;
+Div /= 4;  // Division operator
+console.log(Div)
+
+
+num
+57
+num--
+57
+num
+56
+num
+56
+num++
+56
+++num
+58
+num
+58
+++num
+59
+num
+59
+--num
+58
+num
+58
+num--
+58
+num
+57

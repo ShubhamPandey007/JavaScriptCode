044e2e694ab45a23870f184d075d30857efec6fd
shubham <shubhampandeyup07@gmail.com>
2024-09-09 14:15:27 +0530

    whileLoop code concept

---
+++ practice/Js26whileLoop.js
@@ -0,0 +1,24 @@
+console.log('program started');
+
+
+let i = 10;
+do {
+    console.log(i);
+    i++;
+} while (i < 5) 
+console.log('program Ended');
+
+// 
+let num = 2;
+while (num < 10) {
+    console.log(num);
+    num++;
+
+}
+let num1 = 10;
+for ( let ii = 0; ii <= num1; ii++) {
+    if (ii % 2 !==0){
+        console.log(ii);
+    }
+}
+

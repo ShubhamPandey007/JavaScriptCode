bdaab36b12de282813eb168a253c92f83e532878
shubham <shubhampandeyup07@gmail.com>
2024-09-10 14:28:36 +0530

    function code concept

---
+++ practice/Js29function.js
@@ -0,0 +1,35 @@
+ console.log('Hii');
+//  console.log('My name is Shubham Pandey');
+//  console.log('I am a Web Developer');
+
+function intro() {
+    console.log('Hiii');
+    console.log('My name is Shubham Pandey');
+    console.log("I am a web developer");
+
+    // return 343;
+
+}
+intro();
+
+
+// parameters and arguments
+
+function userdatils ( username , profession, age) {
+    console.log('Hii');
+    console.log(`my name is a ${username}`);
+    console.log(`I am a ${profession}`)
+    console.log(` Hi is ${age} years old`)
+}
+userdatils('Rajan','Software Engineer', 25)
+userdatils('Anurag', 'Mechanical Engineer', 27);
+
+
+////////////////////////
+
+function addNum(a, b) {
+    return a + b;
+}
+let result = addNum(1, 9);
+console.log(result)
+

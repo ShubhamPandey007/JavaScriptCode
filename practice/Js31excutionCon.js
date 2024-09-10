32bac91e33c806981e6c9567897f75712bb78601
shubham <shubhampandeyup07@gmail.com>
2024-09-10 14:40:31 +0530

    excution context concept

---
+++ practice/Js31excutionCon.js
@@ -0,0 +1,17 @@
+
+sayHi();
+var username = 'Shubham';
+var userAge = 30;
+
+function sayHi (){
+   const a = 14;
+   const b = 12;
+   add(7, 9)
+}
+function add (x, y ) {
+    return x + y;
+}
+console.log('program Ended')
+
+
+console.log(username, userAge);

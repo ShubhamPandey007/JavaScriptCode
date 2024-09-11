51f3158d65c524baba94f44d11288e34c462e180
shubham <shubhampandeyup07@gmail.com>
2024-09-11 16:22:20 +0530

    datatype code  concept

---
+++ practice/datatype.js
@@ -0,0 +1,15 @@
+// there are two types of data types primtive and non primtive data types.of
+// number, string, boolean, undefined, null, bigint, Symbol
+
+typeof 7
+typeof "shubham"
+typeof "" // empty string
+typeof NaN
+typeof +'100dkjfdks'
+
+parseInt('1000dfdf')  // we can change string to number
+
+
+
+
+console.log("Hello")

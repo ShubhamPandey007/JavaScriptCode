79600250d3db20bc6aeb0d02bb5b80fcb6094bcf
shubham <shubhampandeyup07@gmail.com>
2024-09-14 23:36:22 +0530

    setTimeout concept code

---
+++ practice/Js37setTimeout.js
@@ -0,0 +1,32 @@
+// set Timeout and set Interval
+
+setTimeout('console.log(true)');
+
+setTimeout('console.log("Hello Shubham");console.log("hello")', 2000); 
+
+
+// write multiple lines to use backticks
+setTimeout(`         
+console.log("Hiiii")
+console.log("I am shubham")
+` , 5000);
+
+setTimeout(`console.log("Hi-1")`, 2000)
+setTimeout(`console.log("hi22")`, 4000)
+setTimeout(`console.log("hiiel33")`, 10000)
+
+//////////
+
+const timer1 = setTimeout(`console.log("Hii-1")`, 1000)
+const timer2 = setTimeout(`console.log("Hii-2")`)
+const timer3 = setTimeout(a, 4000)
+
+const setIntervl = setInterval(a, 3000,)
+clearTimeout(timer2)
+
+function a() {
+    console.log("hello world")
+   
+}
+
+console.log('hello World');
